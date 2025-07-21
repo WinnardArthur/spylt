ebe628aec68007f9e4f32675aba57859be361b45
WinnardArthur <winnardarthur7@gmail.com>
2025-07-21 22:45:51 +0000

    create mesage block section

--- src/components/message-block.jsx
+++ src/components/message-block.jsx
@@ -1,6 +1,68 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap, { SplitText, ScrollSmoother } from "gsap/all";
 
 const MessageBlock = () => {
+  useGSAP(() => {
+    const firstMessageSplit = SplitText.create(".first-message", {
+      type: "words",
+    });
+
+    const secondMessageSplit = SplitText.create(".second-message", {
+      type: "words",
+    });
+
+    const paraSplit = SplitText.create("p", {
+      type: "words, lines",
+      linesClass: "paragraph-line",
+    });
+
+    gsap.to(firstMessageSplit.words, {
+      color: "#faeade",
+      ease: "power1.in",
+      stagger: 1,
+      scrollTrigger: {
+        trigger: ".message-content",
+        scrub: true,
+        start: "top 80%",
+        end: "30% center",
+      },
+    });
+
+    gsap.to(secondMessageSplit.words, {
+      color: "#faeade",
+      stagger: 1,
+      ease: "power1.in",
+      scrollTrigger: {
+        trigger: ".second-message",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+
+    gsap.to(".msg-text-scroll", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "circ.inOut",
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".msg-text-scroll",
+        start: "top 60%",
+      },
+    });
+
+    gsap.from(paraSplit.lines, {
+      stagger: 0.05,
+      yPercent: 310,
+      rotate: 3,
+      ease: "power1.inOut",
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".message-content p",
+        start: "top 60%",
+      },
+    });
+  });
+
   return (
     <section className="message-content">
       <div className="container mx-auto flex-center py-28 relative">
@@ -8,7 +70,10 @@ const MessageBlock = () => {
           <div className="msg-wrapper">
             <h1 className="first-message">Stir up your fearless past and</h1>
 
-            <div className="msg-text-scroll">
+            <div
+              style={{ clipPath: "polygon(0 0, 0 0, 0 100%, 0 100%)" }}
+              className="msg-text-scroll"
+            >
               <div className="bg-light-brown md:pb-5 pb-3 px-5">
                 <h2 className="text-red-brown">Fuel Up</h2>
               </div>
@@ -21,11 +86,11 @@ const MessageBlock = () => {
 
           <div className="flex-center md:mt-20 mt-10">
             <div className="max-w-md px-10 flex-center overflow-hidden">
-                <p>
-
-                Rev up your ebullient spirit and feed the adventure of life with SPYLT, 
-                where you're one chug away from epic nostalgia and fearless fun.
-                </p>
+              <p className="">
+                Rev up your ebullient spirit and feed the adventure of life with
+                SPYLT, where you're one chug away from epic nostalgia and
+                fearless fun.
+              </p>
             </div>
           </div>
         </div>
